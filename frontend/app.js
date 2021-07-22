17645b8f9618feee98ab2600f858b0622330c2d3
ertogrul <michalkordyzon@gmail.com>
2021-07-22 11:38:45 +0200

    1

--- frontend/app.js
+++ frontend/app.js
@@ -3,10 +3,12 @@ var path = require('path');
 var serveStatic = require('serve-static');
 app = express();
 // app.use(serveStatic(__dirname + "/dist"));
+console.log("dirname:::::"+__dirname)
 app.use(serveStatic(__dirname + "/index.html"));
 var port = process.env.PORT || 30000;
+//var port = process.env.PORT || 3000;
 var hostname = '127.0.0.1';
-
+console.log("APP.JS IN frontend")
 app.listen(port, hostname, () => {
    console.log(`Server running at http://${hostname}:${port}/`);
  });
